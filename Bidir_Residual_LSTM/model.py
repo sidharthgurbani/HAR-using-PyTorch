a920619b70ad0078f92d7174e6277f28cacb4fda
Sidharth Gurbani <sidharthgurbani@vpn-pa-full-ptr-81.engr.wisc.edu>
2020-05-02 16:38:13 -0500

    Adding code for Bi-dir Residual LSTM architecure

---
+++ Bidir_Residual_LSTM/model.py
@@ -0,0 +1,106 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+n_classes = 6
+n_input = 9
+n_hidden = 32
+
+n_classes = 6
+n_input = 9
+n_hidden = 32
+
+
+class BiDirResidual_LSTMModel(nn.Module):
+
+    def __init__(self, n_input=n_input, n_hidden=n_hidden, n_layers=2,
+                 n_classes=n_classes, drop_prob=0.5):
+        super(BiDirResidual_LSTMModel, self).__init__()
+
+        self.n_layers = n_layers
+        self.n_hidden = n_hidden
+        self.n_classes = n_classes
+        self.drop_prob = drop_prob
+        self.n_input = n_input
+
+        self.relu1 = nn.Sequential(
+            nn.Linear(n_input, n_input),
+            nn.ReLU()
+        )
+        self.relu2 = nn.Sequential(
+            nn.Linear(n_hidden, n_hidden),
+            nn.ReLU()
+        )
+        self.lstm = nn.LSTM(n_input, n_hidden, n_layers, dropout=self.drop_prob)
+        self.bidir_lstm1 = nn.LSTM(n_input, int(n_hidden / 2), n_layers, bidirectional=True, dropout=self.drop_prob)
+        self.bidir_lstm2 = nn.LSTM(n_hidden, int(n_hidden / 2), n_layers, bidirectional=True, dropout=self.drop_prob)
+        self.fc = nn.Linear(n_hidden, n_classes)
+        self.BatchNorm = nn.BatchNorm1d(64)
+        self.dropout = nn.Dropout(drop_prob)
+
+    def add_residual_component(self, layer1, layer2):
+        return torch.add(layer1, layer2)
+
+    def make_residual_layer(self, input_layer, hidden, first=False):
+        if first:
+            mid_layer, hidden_layer1 = self.bidir_lstm1(input_layer, hidden)
+        else:
+            mid_layer, hidden_layer1 = self.bidir_lstm2(input_layer, hidden)
+
+        mid_layer = self.relu2(mid_layer)
+        output_layer, hidden_layer2 = self.bidir_lstm2(mid_layer, hidden)
+        output_layer = self.relu2(output_layer)
+
+        output = self.add_residual_component(mid_layer, output_layer)
+        output = self.BatchNorm(output)
+        return output
+
+    def forward(self, x, hidden):
+        #print("Shape of input is: {}".format(x.shape))
+        x = x.permute(1, 0, 2)
+
+        x = self.relu1(x)
+        x = self.make_residual_layer(x, hidden, True)
+        x = self.make_residual_layer(x, hidden, False)
+        x = self.dropout(x)
+        #print("Shape of x is: {}".format(x.shape))
+        out = x[-1]
+        out = out.contiguous().view(-1, self.n_hidden)
+        out = self.fc(out)
+        out = F.softmax(out)
+        #print("Shape of out is: {}".format(out.shape))
+
+        return out, hidden
+
+    def init_hidden(self, batch_size):
+        ''' Initialize hidden state'''
+        # Create two new tensors with sizes n_layers x batch_size x n_hidden,
+        # initialized to zero, for hidden state and cell state of LSTM
+        weight = next(self.parameters()).data
+
+        # if (train_on_gpu):
+        if (torch.cuda.is_available()):
+            hidden = (weight.new(2 * self.n_layers, batch_size, int(self.n_hidden / 2)).zero_().cuda(),
+                      weight.new(2 * self.n_layers, batch_size, int(self.n_hidden / 2)).zero_().cuda())
+        else:
+            hidden = (weight.new(2 * self.n_layers, batch_size, int(self.n_hidden / 2)).zero_(),
+                      weight.new(2 * self.n_layers, batch_size, int(self.n_hidden / 2)).zero_())
+
+        return hidden
+
+
+def init_weights(m):
+    if type(m) == nn.LSTM:
+        for name, param in m.named_parameters():
+            if 'weight_ih' in name:
+                torch.nn.init.orthogonal_(param.data)
+            elif 'weight_hh' in name:
+                torch.nn.init.orthogonal_(param.data)
+            elif 'bias' in name:
+                param.data.fill_(0)
+    elif type(m) == nn.Linear:
+        torch.nn.init.orthogonal_(m.weight)
+        m.bias.data.fill_(0)
---
+++ Bidir_Residual_LSTM/train.py
@@ -0,0 +1,80 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+import torch
+from torch import nn
+import numpy as np
+from test import test
+from Functions import extract_batch_size
+
+def train(net, X_train, y_train, X_test, y_test, epochs=100, lr=0.001, weight_decay=0.001):
+    print("\n\n********** Running training! ************\n\n")
+    opt = torch.optim.Adam(net.parameters(), lr=lr, weight_decay=weight_decay)
+    criterion = nn.CrossEntropyLoss()
+
+    #if (train_on_gpu):
+    if (torch.cuda.is_available() ):
+        net.cuda()
+
+    train_losses = []
+    # results = np.empty([0, 5], dtype=np.float32)
+    net.train()
+
+    epoch_train_losses = []
+    epoch_train_acc = []
+    epoch_test_losses = []
+    epoch_test_acc = []
+    for epoch in range(epochs):
+        train_losses = []
+        step = 1
+        batch_size = 64
+
+        h = net.init_hidden(batch_size)
+
+        train_accuracy = 0
+        train_len = len(X_train)
+
+        while step * batch_size <= train_len:
+            batch_xs = extract_batch_size(X_train, step, batch_size)
+            # batch_ys = one_hot_vector(extract_batch_size(y_train, step, batch_size))
+            batch_ys = extract_batch_size(y_train, step, batch_size)
+
+            inputs, targets = torch.from_numpy(batch_xs), torch.from_numpy(batch_ys.flatten('F'))
+            #if (train_on_gpu):
+            if (torch.cuda.is_available() ):
+                inputs, targets = inputs.cuda(), targets.cuda()
+
+            h = tuple([each.data for each in h])
+            opt.zero_grad()
+
+            output, h = net(inputs.float(), h)
+            # print("lenght of inputs is {} and target value is {}".format(inputs.size(), targets.size()))
+            train_loss = criterion(output, targets.long())
+            train_losses.append(train_loss.item())
+
+            top_p, top_class = output.topk(1, dim=1)
+            equals = top_class == targets.view(*top_class.shape).long()
+            train_accuracy += torch.mean(equals.type(torch.FloatTensor))
+            equals = top_class
+
+            train_loss.backward()
+            opt.step()
+            step += 1
+
+        train_loss_avg = np.mean(train_losses)
+        train_accuracy_avg = train_accuracy/(step-1)
+        epoch_train_losses.append(train_loss_avg)
+        epoch_train_acc.append(train_accuracy_avg)
+        print("Epoch: {}/{}...".format(epoch + 1, epochs),
+              ' ' * 16 + "Train Loss: {:.4f}".format(train_loss_avg),
+              "Train accuracy: {:.4f}...".format(train_accuracy_avg))
+        test_loss, test_f1score, test_accuracy = test(net, X_test, y_test, criterion)
+        epoch_test_losses.append(test_loss)
+        epoch_test_acc.append(test_accuracy)
+        if ((epoch+1) % 10 == 0):
+            print("Epoch: {}/{}...".format(epoch + 1, epochs),
+                  ' ' * 16 + "Test Loss: {:.4f}...".format(test_loss),
+                  "Test accuracy: {:.4f}...".format(test_accuracy),
+                  "Test F1: {:.4f}...".format(test_f1score))
+
+    return epoch_train_losses, epoch_train_acc, epoch_test_losses, epoch_test_acc
