a23cc7539be32b9c5854574ab956dec967952070
Sidharth Gurbani <sidharthgurbani@vpn-pa-full-ptr-81.engr.wisc.edu>
2020-05-03 19:35:12 -0500

    change

--- Bidir_Res_LSTM/config.py
+++ Bidir_Res_LSTM/config.py
@@ -4,12 +4,12 @@ n_classes = 6
 n_input = 9
 n_hidden = 32
 
-learning_rate = [0.001, 0.0025, 0.005, 0.01]
+learning_rate = [0.001] #[0.001, 0.0025, 0.005, 0.01]
 weight_decay = 0.0015
 clip_val = 15
 
 batch_size = 100
 drop_prob = 0.3
-n_epochs = 100
+n_epochs = 250
 
 n_layers = 2
