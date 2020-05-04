1d3a1e4ff24ddb5c231875b2f23d78ef8ac9160e
Sidharth Gurbani <sidharthgurbani@Sidharths-MacBook-Pro.local>
2020-05-04 00:59:18 -0500

    change

--- LSTM/config.py
+++ LSTM/config.py
@@ -4,7 +4,7 @@ n_classes = 6
 n_input = 9
 n_hidden = 32
 
-learning_rate = 0.001 #	[0.001, 0.0025, 0.005, 0.01]
+learning_rate = 0.0025 #	[0.001, 0.0025, 0.005, 0.01]
 weight_decay = 0.001
 bidir = False
 
