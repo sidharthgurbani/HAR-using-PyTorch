82610197fd97b211dfb4571a92398dc5dddf3ff4
Sidharth Gurbani <sidharthgurbani@Sidharths-MacBook-Pro.local>
2020-05-04 00:42:49 -0500

    change

--- LSTM/config.py
+++ LSTM/config.py
@@ -4,13 +4,13 @@ n_classes = 6
 n_input = 9
 n_hidden = 32
 
-learning_rate = 0.0025 #	[0.001, 0.0025, 0.005, 0.01]
-weight_decay = 0.0015
+learning_rate = 0.001 #	[0.001, 0.0025, 0.005, 0.01]
+weight_decay = 0.001
 bidir = False
 
 batch_size = 64
 drop_prob = 0.5
-n_epochs = 200
+n_epochs = 100
 n_layers = 2
 
-n_epochs_hold = 120
+n_epochs_hold = 50
