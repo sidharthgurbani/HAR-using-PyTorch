81a3b33b0b2b14bf6441565645db73648fcfae76
Sidharth Gurbani <sidharthgurbani@vpn-pa-full-ptr-81.engr.wisc.edu>
2020-05-04 19:06:26 -0500

    change

--- LSTM/config.py
+++ LSTM/config.py
@@ -7,7 +7,7 @@ n_hidden = 32
 learning_rate = 0.0015 #	[0.001, 0.0025, 0.005, 0.01]
 weight_decay = 0.001
 bidir = 2
-clip_val = 10
+clip_val = 15
 
 batch_size = 64
 drop_prob = 0.5
@@ -15,3 +15,24 @@ n_epochs = 100
 n_layers = 2
 
 n_epochs_hold = 100
+
+
+# Baseline LSTM Architecture:
+# bidir = 0
+# clip_val = 10
+# drop_prob = 0.5
+# n_epochs_hol = 100
+# n_layers = 2
+# learning_rate = 0.0015 #	[0.001, 0.0025, 0.005, 0.01]
+# weight_decay = 0.001
+
+# Bidirectional LSTM Architecture:
+# bidir = 1
+# clip_val = 10
+# drop_prob = 0.5
+# n_epochs_hol = 100
+# n_layers = 2
+# learning_rate = 0.0015 #	[0.001, 0.0025, 0.005, 0.01]
+# weight_decay = 0.001
+
+# 2 layered Bidirectional LSTM Architecture:
